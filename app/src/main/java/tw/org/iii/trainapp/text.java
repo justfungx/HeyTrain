a0eeb3cc97a4fe837db5aca55d5fd0075aee8234
justfungx <justfungx@hotmail.com>
2016-09-21 17:20:36 +0800

    v2

---
+++ app/src/main/java/tw/org/iii/trainapp/text.java
@@ -0,0 +1,11 @@
+package tw.org.iii.trainapp;
+
+/**
+ * Created by user on 2016/9/21.
+ */
+public class text {
+}
+
+//http://huli.logdown.com/posts/206990-teaching-movie-to-nine-fast-development
+
+// https://sheethub.com/data.gov.tw/%E6%94%BF%E5%BA%9C%E8%B3%87%E6%96%99%E9%96%8B%E6%94%BE%E5%B9%B3%E8%87%BA%E8%B3%87%E6%96%99%E9%9B%86%E6%B8%85%E5%96%AE/i/45/JSON
